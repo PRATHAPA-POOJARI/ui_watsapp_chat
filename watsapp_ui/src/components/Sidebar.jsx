4f9fc43bc0fd415804792f8ad7ac2215f38fe29a
prathappoojari607 <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2026-01-11 21:43:19 +0530

    Update Sidebar.jsx

--- watsapp_ui/src/components/Sidebar.jsx
+++ watsapp_ui/src/components/Sidebar.jsx
@@ -1,8 +1,10 @@
 import React, { use } from 'react'
-import { AppBar, Avatar, Box,Toolbar,Typography,Divider, List } from '@mui/material'
-const Sidebar = ({username}) => {
+import { AppBar, Avatar, Box,Toolbar,Typography,Divider, List, ListItemButton, ListItemText } from '@mui/material'
+const Sidebar = ({username,selectedUser}) => {
     const[msg,  setMsg]=React.useState("")
     const[ messages,setMessages]=React.useState([])
+
+    const dummyUsers = ["Rahul", "Sneha", "Amit", "Priya", "John"];
   return (
     <>
     <Box  width="25" borderRadius={"1px solid #ddd"} display='flex' flexDirection='column'></Box>
@@ -21,10 +23,13 @@ const Sidebar = ({username}) => {
         Chats
       </Typography>
           <Divider />
-          <List>
-
-            
-          </List>
+       <List>
+        {dummyUsers.map((user, i) => (
+          <ListItemButton key={i} onClick={() => setSelectedUser(user)}>
+            <ListItemText primary={user} />
+          </ListItemButton>
+        ))}
+      </List>
     </>
   )
 }
