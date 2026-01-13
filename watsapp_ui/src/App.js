8116dae8bc54fc9b35e060baf2700112a3a34d84
prathappoojari607 <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2026-01-13 11:09:09 +0530

    Update App.js

--- watsapp_ui/src/App.js
+++ watsapp_ui/src/App.js
@@ -1,16 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { io } from "socket.io-client";
+import { Box } from '@mui/material';
 import Login from './components/Login';
 import Sidebar from './components/Sidebar';
+import { useState } from 'react';
+import ChatWindow from './components/ChatWindow';
+const socket = io("http://localhost:5000");
 
-function App() {
+
+const App = () => {
+  const [username, setUsername] = useState(null);
+  const [selectedUser, setSelectedUser] = useState("");
+
+  if (!username) return <Login setUsername={setUsername} />;
+
   return (
-    <>
-    
-   <Login/>
-   <Sidebar username="JohnDoe"/>
-    </>
+    <Box display="flex" height="100vh">
+      <Sidebar
+        username={username}
+        selectedUser={selectedUser}
+        setSelectedUser={setSelectedUser}
+      />
+
+      <ChatWindow
+        username={username}
+        selectedUser={selectedUser}
+        socket={socket}
+      />
+    </Box>
   );
-}
+};
 
 export default App;
