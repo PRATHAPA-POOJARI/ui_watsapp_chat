334ed8ba9fec22e2a50faddce3053f8ee2244b81
prathappoojari607 <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2026-01-13 11:09:14 +0530

    Update Login.jsx

--- watsapp_ui/src/components/Login.jsx
+++ watsapp_ui/src/components/Login.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
-import { Box,Paper, Typography } from '@mui/material'  
-const Login = () => {
+import React, { useState } from "react";
+import { Box, Paper, TextField, Button, Typography } from "@mui/material";
+
+const Login = ({ setUsername }) => {
+  const [name, setName] = useState("");
+
+  const handleLogin = () => {
+    if (!name.trim()) return;
+    setUsername(name);
+  };
+
   return (
-    <Box sx={{height:"100vh",
-        display:'flex',
-        justifyContent:'center',
-        bgcolor:"#E0F2F1"
-    }}>
-<Paper sx={{ p:4,width:350,textAlign:'center' }} elevation={3}>
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        bgcolor: "#E0F2F1",
+      }}
+    >
+      <Paper sx={{ p: 4, width: 350, textAlign: "center" }} elevation={3}>
+        <Typography variant="h6" gutterBottom>
+          WhatsApp Messenger
+        </Typography>
 
-<Typography variant='h6' gutterBottom>
+        <TextField
+          fullWidth
+          margin="normal"
+          label="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-Welcome to WhatsApp
-</Typography>
-
-</Paper>
-
+        <Button variant="contained" fullWidth onClick={handleLogin}>
+          Login
+        </Button>
+      </Paper>
     </Box>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
