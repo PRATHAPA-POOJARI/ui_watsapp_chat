959fafd666746c2d27e9d1a35416fed89a449466
prathappoojari607 <prathappoojari607@gmail.com>
2026-04-08 23:47:36 +0530

    Create config.js

---
+++ watsapp_ui/src/components/config.js
@@ -0,0 +1,14 @@
+// src/config.js
+
+// API Base URL
+export const API_BASE_URL = "http://192.168.1.4:5000/api";
+export const SOCKET_URL = "http://192.168.1.4:5000";
+
+// API Endpoints
+export const API_SEND_OTP = `${API_BASE_URL}/auth/send-otp`;
+export const API_VERIFY_OTP = `${API_BASE_URL}/auth/verify-otp`;
+export const API_SEARCH_USERS = `${API_BASE_URL}/chat/search`;
+export const API_CREATE_CHAT = `${API_BASE_URL}/chat`;
+export const API_GET_MESSAGES = (chatId) => `${API_BASE_URL}/chat/message/${chatId}`;
+export const API_SEND_MESSAGE = `${API_BASE_URL}/chat/message`;
+
