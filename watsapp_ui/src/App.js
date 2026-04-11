159fcbc54529feb9dc504792ca1403bafbc27821
prathappoojari607 <prathappoojari607@gmail.com>
2026-04-11 22:53:25 +0530

    Update App.js

--- watsapp_ui/src/App.js
+++ watsapp_ui/src/App.js
@@ -1,41 +1,29 @@
 import './App.css';
-import { io } from "socket.io-client";
 import { Box } from '@mui/material';
 import Login from './components/Login';
-import Sidebar from './components/Sidebar';
-import ChatWindow from './components/ChatWindow';
+import ChatApp from './components/Chatapp';  // ✅ Changed from 'ChatApp' to 'Chatapp'
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
-import { loginUser  } from './Redux/slices/authSlice';
-
-const socket = io("http://192.168.1.4:5000");
+import { loginUser } from './Redux/slices/authSlice';
 
 const App = () => {
-  const dispatch = useDispatch();   // ✅ FIXED
-  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+  const { user, isAuthenticated } = useSelector((state) => state.auth);
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
+    const storedToken = localStorage.getItem("token");
 
-    if (storedUser) {
+    if (storedUser && storedToken) {
       dispatch(loginUser(JSON.parse(storedUser)));
     }
   }, [dispatch]);
 
-  if (!user) return <Login />;
+  if (!isAuthenticated || !user) return <Login />;
 
   return (
     <Box display="flex" height="100vh">
-      <Sidebar
-        username={user.username}
-        selectedUser={user.selectedUser || ""}
-        setSelectedUser={() => {}}
-      />
-      <ChatWindow
-        username={user.username}
-        selectedUser={user.selectedUser || ""}
-        socket={socket}
-      />
+      <ChatApp username={user.username} />
     </Box>
   );
 };
