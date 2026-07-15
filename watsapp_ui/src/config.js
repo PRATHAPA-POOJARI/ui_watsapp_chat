9c519d75d3d2826447e2497934efbe579e560f69
prathappoojari607 <prathappoojari607@gmail.com>
2026-07-15 23:05:36 +0530

    Update config.js

--- watsapp_ui/src/config.js
+++ watsapp_ui/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
-export const SOCKET_URL = "http://192.168.1.2:5000";
-export const API_BASE_URL = "http://192.168.1.2:5000/api";
+export const SOCKET_URL = "http://10.131.251.188:5000";
+export const API_BASE_URL = "http://10.131.251.188/api";
 
 export const API_SEND_OTP = `${API_BASE_URL}/auth/send-otp`;
 export const API_VERIFY_OTP = `${API_BASE_URL}/auth/verify-otp`;
@@ -8,3 +8,5 @@ export const API_SEARCH_USERS = `${API_BASE_URL}/chat/search`;
 export const API_CREATE_CHAT = `${API_BASE_URL}/chat`;
 export const API_GET_MESSAGES = (chatId) => `${API_BASE_URL}/chat/message/${chatId}`;
 export const API_SEND_MESSAGE = `${API_BASE_URL}/chat/message`;
+export const API_UPLOAD = `${API_BASE_URL}/upload`;        // ← ADD THIS
+export const API_REFRESH_URL = `${API_BASE_URL}/upload/refresh-url`; // ← ADD THIS
